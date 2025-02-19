2566d9c10a5cfd024c9cedb473a5b32817e82076
«Elena <LenaRybinskova@gmail.com>
2025-02-19 16:07:54 +0300

    SCRUM-1 add: handle resize width

--- features/SliderClient/Slider.tsx
+++ features/SliderClient/Slider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {Pagination} from 'swiper/modules';
 
@@ -11,19 +11,37 @@ import Image from 'next/image';
 
 type Props = {
     images: Slide[]
+
 }
 
 
-export const SliderClient = ({images}: Props) => {
-    return (
-
-        <Swiper
-            pagination={{clickable: true}}
-            modules={[Pagination]}
-            wrapperClass="mySwiper"
-        >
-            {images.map((image: Slide) => <SwiperSlide className="bg-gray-200 p-6 text-center" key={image.id}><Image
-                src={image.url} alt={image.id} height={212} width={358}/></SwiperSlide>)}
-        </Swiper>
+export const SliderClient = ({ images }: Props) => {
+    const [dynamicWidth, setDynamicWidth] = useState<number>(
+        typeof window !== "undefined" ? window.innerWidth : 0
     );
-}
+
+    useEffect(() => {
+        const handleResize = () => setDynamicWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return (
+        <div>
+            {dynamicWidth < 500 ? (
+                <Swiper pagination={{ clickable: true }} modules={[Pagination]} className="mySwiper">
+                    {images.map((image) => (
+                        <SwiperSlide key={image.id} className="bg-gray-200 p-6 text-center">
+                            <Image src={image.url} alt={`Slide ${image.id}`} height={212} width={358} />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            ) : (
+                <div className="flex gap-4 overflow-x-auto">
+                    LENA
+                </div>
+            )}
+        </div>
+    );
+};
--- features/SliderServer/SliderServer.tsx
+++ features/SliderServer/SliderServer.tsx
@@ -1,15 +1,19 @@
 import {SliderClient} from '@/features/SliderClient/Slider';
 
 
-
-
-const fetchImages=async ()=>{
-    return [{id: '11111', url: "/mocks/slide_1.png"}, {id: '2222', url: "/mocks/slide_2.png"}]
+const fetchImages = async () => {
+    return [
+        {id: '11111', url: '/mocks/slide_1.png'},
+        {id: '2222', url: '/mocks/slide_2.png'},
+        {id: '3333', url: '/mocks/slide_3.png'},
+        {id: '4444', url: '/mocks/slide_4.png'},
+        {id: '5555', url: '/mocks/slide_5.png'},
+        {id: '6666', url: '/mocks/slide_6.png'}]
 }
 
 export default async function SliderServer() {
 
     const images = await fetchImages()
 
-   return <SliderClient images={images}/>
+    return <SliderClient images={images}/>
 }
